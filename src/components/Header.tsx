5da8b8d5533644558d964a78fe9a33719d8f42dc
천성윤 <1112csy@naver.com>
2025-12-03 17:59:32 +0900

    design: 헤더 텍스트 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,17 @@ const Header: React.FC = () => {
     <header className="bg-[#800020] text-white p-4">
       <div className="flex justify-between items-center">
         <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-do-hyeon">
-          광운대학교 정보융합학부 챗봇
+          광운대학교 학과 안내 챗봇
         </h1>
         <a
-          href="https://ic.kw.ac.kr:501/main/main.php"
+          href="https://www.kw.ac.kr/ko/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:opacity-80 transition-opacity"
         >
           <img
             src="/hol.png"
-            alt="IC 홈페이지"
+            alt="광운대학교 홈페이지"
             className="h-8 w-auto sm:h-9 md:h-10 lg:h-12"
           />
         </a>
